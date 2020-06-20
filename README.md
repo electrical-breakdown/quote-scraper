62930d40ae5c8ea2857d90d9bc652d28036b1773
Mike McGovern <mmcgovern5@gmail.com>
2020-06-20 13:29:15 -0600

    Update scrape_quotes function to cache data locally, and update README

--- README.md
+++ README.md
@@ -1,4 +1,25 @@
 # quote-scraper
-A game that scrapes quotes from http://quotes.toscrape.com/, displays a random quote, then asks the player to guess the author. 
+##A little game I made as part of Colt Steele's Modern Python Bootcamp Class
 
-The requests are handled by the built-in Python requsts module, and Beautiful Soup is used to parse the HTML.
+## Descripion
+
+### Main Functions
+The game consists of two main functions: **scrape_quotes** and **start_game**. 
+
+scrape_quotes will request the html from http://quotes.toscrape.com/, extract all the quotes, then add those quotes to a dictionary along with the quote's author and a link to the author's bio page. On first run, quotes will be scraped and then stored locally in a JSON file. On subsequent runs, the scrape_quotes function will check for the existence of the local cache. If quotes have been cached locally, the game will read from the local JSON rather than request and scrape the HTML again. If the local cache hasn't been updated in over 30 days, scraping will take place again and update the JSON with the current quotes. 
+
+start_game will take the list of quotes provided by the scrape_quotes function as an argument, choose a random quote to display to the player, check if the player's guess is correct, and offer a hint if the guess is incorrect. 
+
+### Game Logic
+
+A quote will be selected at random from a list containing all of the available quotes. The player will be given 4 chances to guess the author correctly. After each incorrect guess, a hint will be given. The hints are obtained by scraping the link for the author's about page. the first hint is the author's date of birth, the second hint is the location where the author was born, and the final hint is the author's first and last intial. 
+
+After guessing correctly the player will have the option to play again or quit the game. 
+
+## What I learned
+
+Making this game was a good way to learn about usng Beatiful Soup for web scraping, and the excessive comments were mainly to solidify my understanding of what I was doing. I know it's basic, but it was the first non-trival project I was able to code completely without looking at a tutorial.
+
+Initially I had the scrape_quotes function grab all the author info and add that to a dictionary, but it took way too long for all of those requests to complete. What I ended up doing was just scraping the quotes themselves, and then only requesting the author info for the quote that was randomly selected. This cuts down on the initial wait time for scraping, but means that the game can't be played offline. 
+
+I did some basic checking before trying to write/access the local cache (file permissions, existence of the directory, etc. ), but I'm sure there was probably a more sophisticated way to handle that. I'll explore that more in future projects. 
--- quote_scraper.py
+++ quote_scraper.py
@@ -1,42 +1,110 @@
 import requests
 from bs4 import BeautifulSoup
 from random import choice
+from datetime import datetime, date
+import json
+import csv
+import os
 
-#main url to which relative urls will be appended 
+#main url to which relative urls will be appended
 BASE_URL = "http://quotes.toscrape.com/"
 
-print("Welcome to the random quote game!\n") 
-    
+
+print("Welcome to the random quote game!\n")
+
+
 #function that scrapes the quotes website and collects all the required info
 def scrape_quotes():
-     #find all quote tags on the page and assign them to variable quotes. find_all will return a list
-    quotes = []
+
+    #create empty list to hold all of the quotes
+    all_quotes = []
     page_url = "/page/1"
+
+    #set the folder and filename for the cache file
+    current_dir = os.getcwd()
+    cache_location = os.path.join(current_dir, "cache")
+    cache_file = os.path.join(cache_location, "quotes.json")
+  
+ 
+    #if the local cache already exists, check the age to determine whether or not to update it
+    if os.path.exists(cache_file):
+
     
-
-    #as long as there is a url for the next page, get the url for the next page and request the text
-    while page_url:
-        #request the main page, then request each additional page using the base url plus each page url
-        response = requests.get(BASE_URL+page_url)
+        cache_last_modified = datetime.fromtimestamp(os.path.getmtime(cache_file)).date().day
+        current_date = datetime.today().date().day
+        cache_age = current_date - cache_last_modified
         
-        #show scraping progress for debugging
-        print("loading quotes, please wait...")
+    #if the local cache doesn't exist, create the directory, then create the empty json file
+    else:
+             
+        try:
+            #if not os.path.dirname(local_cache):
+            if not os.path.exists(cache_location):
+                os.mkdir(cache_location)
+                cache_write_access = True
+                
+            with open(cache_file, "w"):
+                cache_write_access = True
+                
+        except PermissionError:
+            #print("Local cache could not be created. Game data will be downloaded instead of read locally.\n")
+            cache_write_access = False
+            pass
+           
+    
+    #check to see if quotes have been cached locally. If there is no saved quotes file, or if it hasn't been updated in 30 days, scrape the quotes again
+    if not os.path.exists(cache_file) or os.stat(cache_file).st_size == 0 or cache_age >= 30:
 
-        #use beautiful soup to parse each page
-        soup = BeautifulSoup(response.text, "html.parser")        
+        #as long as there is a url for the next page, get the url for the next page and request the text
+        while page_url:
+            #request the main page, then request each additional page using the base url plus each page url
+            response = requests.get(BASE_URL+page_url)
+
+            #show scraping progress for debugging
+            print("loading quotes, please wait...")
+
+            #use beautiful soup to parse each page
+            soup = BeautifulSoup(response.text, "html.parser")
+
+            #find all quote tags on each page and assign them to variable page_quotes. find_all will return a list
+            page_quotes = soup.find_all(class_="quote")
+            
+            
+            #parse through each of the quote divs and extract the quote text, author, and about info
+            for item in page_quotes:
+                
+                quote_text = item.find(class_="text").get_text()
+                author_name = item.find(class_="author").get_text()
+                about_url = BASE_URL + item.find("a")["href"]
         
-        #For each new page, find all quotes and add them to the quotes list
-        quotes.extend(soup.find_all(class_="quote"))
+                #create a new dictionary and populate it with the relevent info for each quote    
+                quote_dict = {"quote": quote_text, "author": author_name, "author_about_link": about_url}
 
-        next_btn = soup.find(class_="next")
+                #add each new dictionary to a list that holds all quote dictionaries
+                all_quotes.append(quote_dict)
+               
+            #check to see if there is a "next" button on the page
+            next_btn = soup.find(class_="next")
+            
+            #update next_btn variable with the contents of the next button on the current page. if there is no next button, page_url set to None
+            page_url = next_btn.find("a")["href"] if next_btn else None
 
-        #update next_btn variable with the contents of the next button on the current page
-        page_url = next_btn.find("a")["href"] if next_btn else None
-        
-        
+        #if we have permission to write to the cache folder, write all the quote data to a json file
+        if cache_write_access == True:
+            with open(cache_file, "w") as json_file:
+                json.dump(all_quotes, json_file, indent=2)
 
-    #return the list of quotes
-    return quotes
+
+    #if we don't need to scrape the quotes, read the quote data from the local cache
+    else:
+       
+       #open the local cache and store the contents to the all_quotes list
+        with open(cache_file, "r") as json_file:
+            all_quotes = json.load(json_file)
+
+    
+    return all_quotes
+    
 
 #main game logic
 def start_game(quotes):
@@ -45,16 +113,16 @@ def start_game(quotes):
     random_quote = choice(quotes)
 
     #grab just the text of the quote and assign to variable quote_text
-    quote_text = random_quote.find(class_="text").get_text()
+    quote = random_quote["quote"]
 
     #use the author class to locate the author info
-    author = random_quote.find(class_="author").get_text()
+    author = random_quote["author"]
 
     #in the random quote, find a url which will be a link to the author's bio page
-    about_url = BASE_URL + random_quote.find("a")["href"] 
-
+    #about_url = random_quote["author_about_link"]
+    
     #request the contents of the about page
-    about_response = requests.get(about_url)
+    about_response = requests.get(random_quote["author_about_link"])
 
     #parse the about page with beautiful soup
     about_soup = BeautifulSoup(about_response.text, "html.parser")
@@ -73,8 +141,8 @@ def start_game(quotes):
 
     #display the random quote to the user
     print("\nHere's a random quote: \n")
-    print(quote_text + "\n")
-    
+    print(quote + "\n")
+
     #user starts with 4 guesses
     guesses_remaining = 4
 
@@ -85,11 +153,11 @@ def start_game(quotes):
     while guesses_remaining > 0 :
         guess = input("Who said this? ")
 
-        # if guess is right, game ends and loop breaks out            
+        # if guess is right, game ends and loop breaks out
         if guess.lower() == author.lower():
             print("That's correct!")
             break
-            
+
         # if guess is incorrect, and they haven't used all their hints, give the use a hint. After each incorrect guess a different hint will be displayed by iterating through the hints list
         elif guess.lower() != author.lower() and guesses_remaining > 1:
             guesses_remaining -= 1
@@ -97,7 +165,7 @@ def start_game(quotes):
             print(hints[hint_index] + "\n")
             hint_index += 1
 
-        else: 
+        else:
             guesses_remaining -= 1
             print(f"\nSorry! You lose! The correct answer was {author}.\n")
 
@@ -113,10 +181,11 @@ def start_game(quotes):
         return start_game(quotes)
     else:
         print("\nThanks for playing!")
-            
-            
+
+
 #call the scrape quotes funtcion to populate the quotes list
 quotes = scrape_quotes()
+
 
 #call the main game function
 start_game(quotes)
